4d267c259eb386b5b8d44604f727f3927379eadc
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-11-21 14:28:04 +0700

    :bento: Edited form appearance

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -27,15 +27,15 @@ Listing
   {{ csrf_field() }}
   <div class="form-group">
     <label for="exampleInputEmail1">Citizen ID</label>
-    <input class="form-control" id="id" name="id" placeholder="1234567890123">
+    <input class="form-control" id="id" name="id" placeholder="1234567890123" autofocus>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Internet Username</label>
+    <input class="form-control" id="login_id" name="login_id" placeholder="12345">
   </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Name</label>
     <input class="form-control" id="name" name="name" placeholder="John Smith">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Internet Username</label>
-    <input class="form-control" id="login_id" name="login_id" placeholder="u0000000000">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
