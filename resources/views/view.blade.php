bd7bf3ae46b41665db0ed66d22db69746df7ca5a
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-11-25 09:45:56 +0700

    :bento: Tabular look in registration view

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -5,9 +5,25 @@ Listing
 @endsection
 
 @section('content')
-<h1>Details: {{ $registration['id'] }}</h1>
-<h2>Name</h2>
-<p>{{ $registration['name'] }}</p>
-<h2>Assigned KUWIN Login</h2>
-<p>{{ $registration['login_id'] }}</p>
+<h1>Registration Details</h1>
+<table class="table">
+  <tbody>
+    <tr>
+      <th scope="row">Citizen ID</th>
+      <td>{{ $registration['id'] }}</td>
+    </tr>
+    <tr>
+      <th scope="row">Name</th>
+      <td>{{ $registration['name'] }}</td>
+    </tr>
+    <tr>
+      <th scope="row">KU Login ID</th>
+      <td>{{ $registration['login_id'] }}</td>
+    </tr>
+    <tr>
+      <th scope="row">Phone</th>
+      <td>{{ $registration['phone'] }}</td>
+    </tr>
+  </tbody>
+</table>
 @endsection
